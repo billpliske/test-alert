04f49b7eeeb70fcb4163ba0bdedd71a94ec9b4e3
Bill Pliske <bpliske@azcentral.com>
2017-06-05 22:12:42 -0700

    removed inline style

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,16 +5,15 @@ import './Contact.css';
 class Contact extends Component {
     
     render() {
-
         return ( 
             <div class="form">
                 <form id="contactform" method="POST">
                     <input type="text" name="name" placeholder="Your name" />
                     <input type="text" name="_replyto" placeholder="Your email address" />
-                    <input type="text" name="_gotcha" style="display:none" />
+                    <input type="text" name="_gotcha" />
                     <textarea rows="4" type="text" name="question" placeholder="What does your business need?"></textarea>
                     <input type="hidden" name="_next" value="http://www.billpliske.com/thankyou.html" />
-                    <input type="submit" value="Submit" class="mailbtn" />
+                    <input type="submit" value="Submit" className="mailbtn" />
                 </form>
             </div>
         );
